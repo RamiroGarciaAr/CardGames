0c00bf73c2c7424a00d2f8dae3d5e856899e5a44
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2024-06-14 00:02:04 -0300

    Basic Project

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,10 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Card of Heroes");
-		config.setWindowedMode(800, 480);
-		config.useVsync(true);
-
+		config.setTitle("CardGame");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
